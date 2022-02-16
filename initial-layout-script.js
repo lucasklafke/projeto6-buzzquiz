4231ce12cd0ce546c24b0052cc2cfba4141b5cd6
Lucas klafke <lucasnkz11@gmail.com>
2022-02-16 15:56:13 -0300

    quiz-header e função responsável pela request do quiz criados

--- initial-layout-script.js
+++ initial-layout-script.js
@@ -8,13 +8,13 @@ function renderQuiz(response){
     const main = document.querySelector(".quizz-container")
     for(i = 0; i < infos.length; i++){
         main.innerHTML += 
-        `<div class="quizz" onclick="enterQuizz">
-            <img src='${infos[i].image}' alt="" onclick="enterQuizz()">
+        `<div class="quizz" id="${infos[i].id}" onclick="enterQuizz(this)">
+            <img src='${infos[i].image}' alt="">
             <h3>${infos[i].title}</h3>
         </div>
         `
     }
-    console.log(infos)
-    console.log(infos.response)
+    //console.log(infos)
+    //console.log(infos.response)
 }
 getQuizzes()
--- playQuizz.js
+++ playQuizz.js
@@ -1,4 +1,17 @@
-function enterQuizz(){
+function enterQuizz(quizz){
     const initialLayout = document.querySelector(".initial-layout").style.display = "none"
-    window.alert("oi")
+    getQuizz(quizz)
+}
+function getQuizz(quizz){
+    const id = quizz.id
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`)
+    promise.then(renderQuizzLayout)
+    promise.catch()
+}
+function renderQuizzLayout(response){
+    console.log('deubom')
+    console.log(response.data)
+}
+function errorRenderQuizzLayout(response){
+    console.log(response.response)
 }
